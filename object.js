f85a585940df0b9c2c6069c46fcbd0520051e56a
chaitanya200e <chaitanyamandale125@gmail.com>
2026-03-24 16:55:43 +0530

    i commit

---
+++ object.js
@@ -0,0 +1,42 @@
+// const object = {
+//    name : "chaitanya",
+//    age : 21,
+// }
+
+// const object = {
+//    name : "chaitanya",
+//    age : 21,
+//    gender : "male",
+// };
+
+// console.log(object)
+
+// object.age = 26;
+
+// console.log(object["age"]);
+
+// console.log(object["name"]);
+
+// const data ={
+//     device_id : "abcdef",
+//     temperature: 37,
+//     location :{
+//         lat : "37.48.63.48",
+//         lon : "48.54.7.6",
+//     }
+// }
+// console.log(data.location.lat)
+
+// const data = {
+//     name : 'chaitanya',
+//     func : () => {
+//         console.log("hello")
+//     }
+
+// }
+// console.log(data)
+// data.func();
+
+// const {name , func}= data;
+// func();
+// console.log(name);
